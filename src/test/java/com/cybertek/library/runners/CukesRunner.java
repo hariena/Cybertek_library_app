1b3525fd947f2417d501f018a9d4cb9e88be31f8
harienabrhane <Haru41992>
2022-01-20 13:02:04 -0600

    Initial commit

---
+++ src/test/java/com/cybertek/library/runners/CukesRunner.java
@@ -0,0 +1,11 @@
+package com.cybertek.library.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions
+public class CukesRunner {
+
+}
